f4b359fdc12f5196e42ae45fb4f988a3896731ec
jacob <1063010784@qq.com>
2024-09-30 17:32:36 +0800

    bulid

--- src/LayerPopupModal.ts
+++ src/LayerPopupModal.ts
@@ -2,9 +2,14 @@
 import { Modal,App} from 'obsidian';
 import { createApp, App as VueApp } from 'vue';
 import Popup from './LayerPopupModal.vue';
+import { exec, execSync } from 'child_process';
+import { ref, Ref, watch } from 'vue';
+
+const vaultPath = 'D:\\code_workspace\\xmind-obsidian\\xmind-obsidian\\md';
 class LayerPopupModal extends Modal {
     vueapp: VueApp;
-
+    hello: () => void;
+    runingState: Ref<boolean>;
     constructor(app: App) {
         super(app);
     }
@@ -16,15 +21,91 @@ class LayerPopupModal extends Modal {
         let content = container.createEl("div", {
             cls: "my-plugin-view"
         });
-        this.vueapp = createApp(Popup);
+        this.runingState = ref(false)
+        this.vueapp = createApp(Popup, {
+            props: {
+                runingState: this.runingState
+            }
+        });
         this.vueapp.mount(content);
+        // 监听popup-container的compile-btn
+        const compileBtn = content.querySelector('.compile-btn');
+        compileBtn.addEventListener('click', async () => {
+            this.runingState.value = true
+            await this.runNodeCLI()
+            this.runingState.value = false
+        })
+        watch(() => this.runingState.value, (newVal, oldVal) => {
+            console.log('newVal', newVal)
+            console.log('oldVal', oldVal)
+        })
     }
 
     onClose() {
         let { contentEl } = this;
         contentEl.empty();
     }
+
+    async installContentlayer() {
+        try {
+            const command = 'npm';
+            const args = ['install', '-g', 'contentlayer'];
+            const child = exec(`${command} ${args.join(' ')}`);
+            // 正在运行emoji
+            const runningEmoji = '🚀';
+            console.log(`${runningEmoji} 正在安装contentlayer...`);
+            // 监听输出
+            child.stdout.on('data', (data) => {
+                console.log(`${data}`);
+            });
+
+            child.stderr.on('data', (data) => {
+                console.error(`${data}`);
+            });
+
+            child.on('close', (code) => {
+                console.log(`安装contentlayer完成，代码: ${code} 🎉`);
+            });
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+
+    async runNodeCLI() {
+        console.log('building...')
+        // 如果不存在全局安装contentlayer，则先全局安装contentlayer，之后build
+        try {
+            try {
+                const result = execSync('contentlayer --version', { encoding: 'utf8' });
+                console.log(`contentlayer version: ${result} 🚀`);
+            } catch (error) {
+                console.error(`Error: ${error.message}`);
+            }
+        } catch (error) {
+            await this.installContentlayer()
+        }
+
+        // const command = 'contentlayer build';
+        // 打印路径
+        // const cdCommand = process.platform === 'win32' ? 'echo %cd%' : 'pwd'; // Updated command
+        const cdCommand = process.platform === 'win32' ? `cd "${vaultPath}"` : `cd '${vaultPath}'`; 
+        const command = `${cdCommand} && contentlayer build`
+        try {
+            const result = await new Promise((resolve, reject) => {
+                exec('npx contentlayer build', { encoding: 'utf8', shell: process.platform === 'win32' ? 'cmd.exe' : '/bin/bash' }, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(`${error.message}\n${error.stack}`);
+                        return;
+                    }
+                    resolve(stdout);
+                });
+            });
+            console.log(`stdout: ${result}`);
+            console.log('building done')
+        } catch (error) {
+            console.log('build error', error)
+        }
+    }
 }
 
 export default LayerPopupModal;
-
--- src/LayerPopupModal.vue
+++ src/LayerPopupModal.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h2>更新博客</h2>
+  <div class="popup-container">
+    <button class="compile-btn" @click="compileBlog" :disabled="runingState">{{ runingState ? 'building...' : 'compile 博客' }}</button>
+    <h3>编译的文件列表</h3>
     <ul>
       <li v-for="post in posts" :key="post._id">{{ post._id }}</li>
     </ul>
@@ -9,14 +10,45 @@
 
 <script setup lang="tsx">
 import { allPosts } from '../.contentlayer/generated/index.mjs';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
+const props = defineProps({
+  runingState: {
+    type: Boolean,
+    required: true
+  }
+});
+
+console.log('props', props)
 
 const posts = ref(allPosts)
+
+// const compileBlog = () => {
+//   // 调用 LayerPopupModal 的 runNodeCLI 方法
+//   props.runingState = true
+// }
+// watch(() => props.runingState, (newVal, oldVal) => {
+//   console.log('newVal', newVal)
+//   console.log('oldVal', oldVal)
+// }, { immediate: true })
 </script>
 
 
 <style scoped>
 h2 {
   color: lightcoral;
+}
+
+.compile-btn {
+  background-color: lightcoral;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  float: right;
+}
+
+.popup-container {
+  padding: 5px 0;
 }
 </style>
